0e9596d29aee1b4ba80757235976540734039451
Darrel C <dcastellano2007@gmail.com>
2019-08-23 23:24:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
